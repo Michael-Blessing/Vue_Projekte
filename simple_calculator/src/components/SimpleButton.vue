946bf68bcf0b89e04c4113b1859ad6db850ea603
Michael-Blessing <michael.blessing005@gmail.com>
2022-12-07 08:34:20 +0100

    styling changes

--- simple_calculator/src/components/SimpleButton.vue
+++ simple_calculator/src/components/SimpleButton.vue
@@ -23,12 +23,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.button {
-  width:100px;
-  padding:10px;
-  border-radius: 10px;
-}
-</style>
