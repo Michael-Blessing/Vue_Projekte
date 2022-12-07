82736da3b52a069728b85aa32ea39c4b887f88ef
Michael-Blessing <michael.blessing005@gmail.com>
2022-12-07 08:21:18 +0100

    Vue Calculator

---
+++ simple_calculator/src/components/SimpleButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="calculate">
+    {{beschriftung}}
+  </button>
+</template>
+
+<script>
+
+
+export default {
+  emits: ['calculate'],
+  name:'SimpleButton',
+  props: {
+    beschriftung: String
+  },
+  data() {
+    return [];
+  },
+  methods: {
+    calculate() {
+      this.$emit('calculate', this.beschriftung);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.button {
+  width:100px;
+  padding:10px;
+  border-radius: 10px;
+}
+</style>
